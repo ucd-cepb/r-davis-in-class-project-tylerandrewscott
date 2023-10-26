b4bab781f75cca8112e5f25c318a50302e1e19a3
Tyler Scott <tascott@ucdavis.edu>
2023-10-26 14:15:57 -0700

    tyler in class conditonal statement video

--- video_scripts/Week4_ConditionalStatements.R
+++ video_scripts/Week4_ConditionalStatements.R
@@ -3,25 +3,12 @@ surveys <- read_csv("data/portal_data_joined.csv")
 
 summary(surveys$hindfoot_length)
 
-ifelse(test or condition that must be met, what to do if test is met, what to do if test is not met)
+ifelse(test or condition, waht to do if yes/true, what to do if no/false)
+surveys$hindfoot_length < 29.29
+surveys$hindfoot_cat <- ifelse(test = surveys$hindfoot_length < 29.29,yes = "small",no = "big")
 
-
-ifelse(test = surveys$hindfoot_length < 29.29,yes = "small",no = "big")
-
-test <- F
-
-if(test==TRUE){print('this is true')}else{'this is false!'}
-
-
-surveys$hindfoot_cat <- ifelse(test = surveys$hindfoot_length < 29.29,
-                               yes = "small",no = "big")
-
-head(surveys$hindfoot_cat,100)
+head(surveys$hindfoot_cat)
 head(surveys$hindfoot_length)
-
-ifelse(surveys$hindfoot_length<20,yes = 'small',
-       ifelse(surveys$hindfoot_length<30,yes = 'medium',no = 'big'))
-
 
 surveys %>% 
   mutate(hindfoot_cat = case_when(
@@ -33,13 +20,10 @@ surveys %>%
 
 NA_character_
 NA_integer_
-NA_character_
-
 
 surveys %>% 
   mutate(hindfoot_cat = case_when(
     hindfoot_length > 29.29 ~ "big",
-    hindfoot_length > 20 ~ 'medium',
     is.na(hindfoot_length) ~ NA_character_,
     TRUE ~ "small"
   )) %>% 
